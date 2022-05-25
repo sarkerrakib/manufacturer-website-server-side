0c49a179bfa95dd84abb2cd166d7d21ae007208c
sarkerrakib <freelancerrakib25@gmail.com>
2022-05-25 15:04:10 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect();
+        console.log('Database')
+    }
+    finally{
 
+    }
+
+}
 
 run().catch(console.dir);
 
